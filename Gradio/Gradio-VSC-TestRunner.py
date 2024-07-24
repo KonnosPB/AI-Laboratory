799a7c8d70a55aea050d01a876bfb356f2d67f72
Papoulas, Konstantin <konstantin.papoulas@kumavision.com>
2024-07-24 14:30:30 +0200

    MED-12340 feat(gradio): add save/load chat history
    Why: Enhance usability by preserving chat history.
    
    How:
    🛠️ Gradio/Gradio-VSC-TestRunner.py ->
    - Added tkinter-based save/load functionality
    - Improved file loading by file extension detection

--- Gradio/Gradio-VSC-TestRunner.py
+++ Gradio/Gradio-VSC-TestRunner.py
@@ -3,12 +3,27 @@ import gradio as gr
 import pprint
 from openai import AzureOpenAI
 import asyncio
+import tkinter as tk
+from tkinter import filedialog
+import json
 
 grhistory = []
+
 def load_file_into_history(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:        
         filecontent =  file.read()
-        grhistory.append([filecontent, "Ok."])
+        file_name, file_extension = os.path.splitext(file_path)   
+        print(f"#file extension: {file_extension}")     
+        if file_extension == ".ts":
+            grhistory.append([f"``` typescript\n{filecontent}\n```", "Ok."])
+        elif file_extension == ".json":
+            grhistory.append([f"``` json\n{filecontent}\n```", "Ok."])
+        elif file_extension == ".xml":
+            grhistory.append([f"``` xml\n{filecontent}\n```", "Ok."])
+        elif file_extension == ".ps1":
+            grhistory.append([f"``` powershell\n{filecontent}\n```", "Ok."])
+        else:
+            grhistory.append([filecontent, "Ok."])
 
 load_file_into_history('C:/Repos/Github/KonnosPB/AI-Laboratory/Prompts/Introdution-VSC-BusinesCentral-TestRunner.txt')
 load_file_into_history('C:/Repos/Github/KonnosPB/BusinessCentral-AL-Test-Runner-App/src/codeunit/WebApi.Codeunit.al')
@@ -19,48 +34,75 @@ load_file_into_history('C:/Repos/Github/KonnosPB/VSC-BusinessCentral-Test-Runner
 load_file_into_history('C:/Repos/Github/KonnosPB/VSC-BusinessCentral-Test-Runner/src/testViewProviders.ts')
 load_file_into_history('C:/Repos/Github/KonnosPB/VSC-BusinessCentral-Test-Runner/src/extension.ts')
 
-#Konvertiert eine Gradio-History in das Azure OpenAI-Nachrichtenformat.
 def gradio_history_to_azure_openai_messages(gradio_history):    
     messages = []   
     with open("C:/Repos/Github/KonnosPB/AI-Laboratory/Prompts/SystemPrompt-VSC-BusinesCentral-TestRunner.txt", 'r', encoding='utf-8') as file:
         filecontent =  file.read()
         messages.append({"role": "system", "content": filecontent})
     for user_input, model_response in gradio_history:
-        # Füge die Benutzer-Eingabe als Nachricht hinzu
         messages.append({"role": "user", "content": user_input})
-        # Füge die Modell-Antwort als Nachricht hinzu
         messages.append({"role": "assistant", "content": model_response})    
     return messages
 
 async def chat(message, history):
-    #print("#message");pprint.pprint(message)    
-    #print("#gradio-history"); pprint.pprint(history)    
     messages = gradio_history_to_azure_openai_messages(history)    
-    #print("#az-history");pprint.pprint(message)    
     prompt = message
     if (hasattr(message, "text")):
         prompt = message.text
-    #print("#prompt");pprint.pprint(prompt)  
     messages.append({"role": "user", "content": prompt})
     response = llm.chat.completions.create(model='gpt-4o', messages=messages, stream=True, temperature=0.5) 
-    #print(f"#response");pprint.pprint(response)     
     aimessage = ""
     for chunk in response:
-        ##print(f"#chunk");pprint.pprint(chunk)  
         if len(chunk.choices) > 0:
             delta = chunk.choices[0].delta
-            ##print(f"#delta");pprint.pprint(delta)  
-            if delta.content: # Get remaining generated response if applicable                
+            if delta.content:                
                 aimessage += delta.content
                 yield aimessage
 
+def save_history():
+    print("#save_history")
+    root = tk.Tk()
+    root.withdraw()  # Versteckt das Hauptfenster
+    file_path = filedialog.asksaveasfilename(defaultextension=".json", filetypes=[("JSON files", "*.json"), ("All files", "*.*")])    
+    print(f"#file_path {file_path}");
+    if file_path:
+        with open(file_path, 'w', encoding='utf-8') as file:
+            json.dump(grhistory, file, ensure_ascii=False, indent=4)
+    root.destroy()
+
+def load_history():
+    root = tk.Tk()
+    root.withdraw()  # Versteckt das Hauptfenster
+    file_path = filedialog.askopenfilename(filetypes=[("JSON files", "*.json"), ("All files", "*.*")])
+    if file_path:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            new_history = json.load(file)
+            grhistory.clear()
+            grhistory.extend(new_history)
+    root.destroy()
+    return grhistory
+
 llm = AzureOpenAI(azure_deployment='gpt-4o')
-chatbot = gr.Chatbot(value=grhistory, height="100%")
-demo = gr.ChatInterface(    
-    fn=chat,    
-    chatbot=chatbot,
-    title="Development Bot",
-    autofocus=True,
-    multimodal=True,
-)
+chatbot = gr.Chatbot(value=grhistory, height="100%", min_width="100%", render_markdown=True, bubble_full_width=True, show_copy_button=True)
+
+with gr.Blocks() as demo:
+    gr.Markdown("# Development Bot")
+    chatbot.render()    
+    with gr.Row():
+        save_button = gr.Button("Save Chat History")
+        load_button = gr.Button("Load Chat History")
+
+    save_button.click(fn=save_history)
+    load_button.click(fn=load_history, outputs=chatbot)
+
+    gr.ChatInterface(
+        fn=chat,
+        chatbot=chatbot,
+        title="Development Bot",
+        autofocus=True,
+        multimodal=True,
+        fill_width=True,
+        fill_height=True,        
+    )
+
 demo.launch()
